2301130aaf2f140d50237c97b7fe7ecf208bed99
serega404 <ppoocpel8888@gmail.com>
2021-10-02 18:35:42 +0300

    Added switch sounds

--- Toggle/Assets/Scripts/ToggleController.cs
+++ Toggle/Assets/Scripts/ToggleController.cs
@@ -4,149 +4,173 @@ using UnityEngine.UI;
 
 public class ToggleController : MonoBehaviour
 {
-	public bool isOn;
+    public bool isOn;
 
-	public Color onColorBg;
-	public Color offColorBg;
+    public Color onColorBg;
+    public Color offColorBg;
 
-	public Image toggleBgImage;
-	public RectTransform toggle;
+    public AudioSource audioSource;
 
-	public GameObject handle;
-	private RectTransform handleTransform;
+    public Image toggleBgImage;
+    public RectTransform toggle;
 
-	private float handleSize;
-	private float onPosX;
-	private float offPosX;
+    public GameObject handle;
+    private RectTransform handleTransform;
 
-	public float handleOffset;
+    private float handleSize;
+    private float onPosX;
+    private float offPosX;
 
-	public GameObject onIcon;
-	public GameObject offIcon;
+    public float handleOffset;
 
-	public float speed;
-	static float t = 0.0f;
+    public GameObject onIcon;
+    public GameObject offIcon;
 
-	private bool switching = false;
+    public float speed;
+    static float t = 0.0f;
 
-	[Space(20)]
-	public UnityEvent OnAction;
-	public UnityEvent OffAction;
+    private bool switching = false;
 
-	void Awake()
-	{
-		handleTransform = handle.GetComponent<RectTransform>();
-		RectTransform handleRect = handle.GetComponent<RectTransform>();
-		handleSize = handleRect.sizeDelta.x;
-		float toggleSizeX = toggle.sizeDelta.x;
-		onPosX = (toggleSizeX / 2) - (handleSize / 2) - handleOffset;
-		offPosX = onPosX * -1;
-	}
+    public AudioClip OnSound;
+    public AudioClip OffSound;
 
-	void Start()
-	{
-		if (isOn)
-		{
-			toggleBgImage.color = onColorBg;
-			handleTransform.localPosition = new Vector3(onPosX, 0f, 0f);
-			onIcon.gameObject.SetActive(true);
-			offIcon.gameObject.SetActive(false);
-		}
-		else
-		{
-			toggleBgImage.color = offColorBg;
-			handleTransform.localPosition = new Vector3(offPosX, 0f, 0f);
-			onIcon.gameObject.SetActive(false);
-			offIcon.gameObject.SetActive(true);
-		}
-	}
+    [Space(20)]
+    public UnityEvent OnAction;
+    public UnityEvent OffAction;
 
-	void Update()
-	{
-		if (switching)
-		{
-			Toggle(isOn);
-		}
-	}
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        handleTransform = handle.GetComponent<RectTransform>();
+        RectTransform handleRect = handle.GetComponent<RectTransform>();
+        handleSize = handleRect.sizeDelta.x;
+        float toggleSizeX = toggle.sizeDelta.x;
+        onPosX = (toggleSizeX / 2) - (handleSize / 2) - handleOffset;
+        offPosX = onPosX * -1;
+    }
 
-	public void DoYourStaff()
-	{
-		if (isOn)
-			OnAction.Invoke();
-		else
-			OffAction.Invoke();
-	}
+    void Start()
+    {
+        if (isOn)
+        {
+            toggleBgImage.color = onColorBg;
+            handleTransform.localPosition = new Vector3(onPosX, 0f, 0f);
+            onIcon.gameObject.SetActive(true);
+            offIcon.gameObject.SetActive(false);
+        }
+        else
+        {
+            toggleBgImage.color = offColorBg;
+            handleTransform.localPosition = new Vector3(offPosX, 0f, 0f);
+            onIcon.gameObject.SetActive(false);
+            offIcon.gameObject.SetActive(true);
+        }
+    }
 
-	public void Switching()
-	{
-		switching = true;
-	}
+    void Update()
+    {
+        if (switching)
+        {
+            Toggle(isOn);
+        }
+    }
 
-	public void Toggle(bool toggleStatus)
-	{
-		if (!onIcon.active || !offIcon.active)
-		{
-			onIcon.SetActive(true);
-			offIcon.SetActive(true);
-		}
+    public void DoYourStaff()
+    {
+        if (isOn)
+            OnAction.Invoke();
+        else
+            OffAction.Invoke();
+    }
 
-		if (toggleStatus)
-		{
-			toggleBgImage.color = SmoothColor(onColorBg, offColorBg);
-			Transparency(onIcon, 1f, 0f);
-			Transparency(offIcon, 0f, 1f);
-			handleTransform.localPosition = SmoothMove(handle, onPosX, offPosX);
-		}
-		else
-		{
-			toggleBgImage.color = SmoothColor(offColorBg, onColorBg);
-			Transparency(onIcon, 0f, 1f);
-			Transparency(offIcon, 1f, 0f);
-			handleTransform.localPosition = SmoothMove(handle, offPosX, onPosX);
-		}
+    public void Switching()
+    {
+        if (!switching)
+        {
+            switching = true;
+            if (isOn) // to left
+            {
+                if (OffSound != null)
+                {
+                    audioSource.clip = OffSound;
+                    audioSource.Play();
+                }
+            }
+            else
+            {
+                if (OnSound != null)
+                {
+                    audioSource.clip = OnSound;
+                    audioSource.Play();
+                }
+            }
+        }
+    }
 
-	}
-	Vector3 SmoothMove(GameObject toggleHandle, float startPosX, float endPosX)
-	{
+    public void Toggle(bool toggleStatus)
+    {
+        if (!onIcon.active || !offIcon.active)
+        {
+            onIcon.SetActive(true);
+            offIcon.SetActive(true);
+        }
 
-		Vector3 position = new Vector3(Mathf.Lerp(startPosX, endPosX, t += speed * Time.deltaTime), 0f, 0f);
-		StopSwitching();
-		return position;
-	}
-	Color SmoothColor(Color startCol, Color endCol)
-	{
-		Color resultCol;
-		resultCol = Color.Lerp(startCol, endCol, t += speed * Time.deltaTime);
-		return resultCol;
-	}
-	CanvasGroup Transparency(GameObject alphaObj, float startAlpha, float endAlpha)
-	{
-		CanvasGroup alphaVal;
-		alphaVal = alphaObj.gameObject.GetComponent<CanvasGroup>();
-		alphaVal.alpha = Mathf.Lerp(startAlpha, endAlpha, t += speed * Time.deltaTime);
-		return alphaVal;
-	}
-	void StopSwitching()
-	{
-		if (t > 1.0f)
-		{
-			switching = false;
+        if (toggleStatus)
+        {
+            toggleBgImage.color = SmoothColor(onColorBg, offColorBg);
+            Transparency(onIcon, 1f, 0f);
+            Transparency(offIcon, 0f, 1f);
+            handleTransform.localPosition = SmoothMove(handle, onPosX, offPosX);
+        }
+        else
+        {
+            toggleBgImage.color = SmoothColor(offColorBg, onColorBg);
+            Transparency(onIcon, 0f, 1f);
+            Transparency(offIcon, 1f, 0f);
+            handleTransform.localPosition = SmoothMove(handle, offPosX, onPosX);
+        }
 
-			t = 0.0f;
-			switch (isOn)
-			{
-				case true:
-					isOn = false;
-					DoYourStaff();
-					break;
+    }
+    Vector3 SmoothMove(GameObject toggleHandle, float startPosX, float endPosX)
+    {
+        Vector3 position = new Vector3(Mathf.Lerp(startPosX, endPosX, t += speed * Time.deltaTime), 0f, 0f);
+        StopSwitching();
+        return position;
+    }
+    Color SmoothColor(Color startCol, Color endCol)
+    {
+        Color resultCol;
+        resultCol = Color.Lerp(startCol, endCol, t += speed * Time.deltaTime);
+        return resultCol;
+    }
+    CanvasGroup Transparency(GameObject alphaObj, float startAlpha, float endAlpha)
+    {
+        CanvasGroup alphaVal;
+        alphaVal = alphaObj.gameObject.GetComponent<CanvasGroup>();
+        alphaVal.alpha = Mathf.Lerp(startAlpha, endAlpha, t += speed * Time.deltaTime);
+        return alphaVal;
+    }
+    void StopSwitching()
+    {
+        if (t > 1.0f)
+        {
+            switching = false;
 
-				case false:
-					isOn = true;
-					DoYourStaff();
-					break;
-			}
+            t = 0.0f;
+            switch (isOn)
+            {
+                case true:
+                    isOn = false;
+                    DoYourStaff();
+                    break;
 
-		}
-	}
+                case false:
+                    isOn = true;
+                    DoYourStaff();
+                    break;
+            }
+
+        }
+    }
 
 }
